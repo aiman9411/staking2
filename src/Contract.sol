990875ed4071c576aeb56d79b1bb0b966279200a
Aiman Nazmi <aimannazmi59@gmail.com>
2022-08-08 00:06:30 +0800

    add rewardPerToken function

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin/contracts/security/ReentrancyGaurd.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Contract {}
+error TransferFailed();
+error NeedsMoreThanZero();
+
+contract Staking is ReentrancyGuard {
+    IERC20 public s_rewardsToken;
+    IERC20 public s_stakingToken;
+
+    uint256 public constant REWARD_RATE = 100;
+    uint256 public s_lastUpdateTime;
+    uint256 public s_rewardPerTokenStored;
+
+    mapping(address => uint256) public s_userRewardPerTokenPaid;
+    mapping(address => uint256) public s_rewards;
+
+    uint256 private s_totalSupply;
+    mapping(address => uint256) public s_balances;
+
+    event Stake(address indexed user, uint256 indexed amount);
+    event Unstake(address indexed user, uint256 indexed amount);
+    event RewardClaimed(address indexed user, uint256 indexed amount);
+
+    constructor(address stakingToken, address rewardsToken) {
+        s_stakingToken = IERC20(stakingToken);
+        s_rewardsToken = IERC20(rewardsToken);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (s_totalSupply == 0) {
+            return s_rewardPerTokenStored;
+        }
+        return s_rewardPerTokenStored + (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalSupply);
+    }
+
+    function stake(uint256 amount) external updateReward(msg.sender) nonReentrant moreThanZero(amount) {
+
+    }
+
+    modifier updateReward(address account) {
+
+    }
+}
