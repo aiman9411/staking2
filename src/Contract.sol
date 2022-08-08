f9952b9172d7fdeb98df5d3dc3aeaee2372c33ec
Aiman Nazmi <aimannazmi59@gmail.com>
2022-08-08 16:16:02 +0800

    add more functions

--- src/Contract.sol
+++ src/Contract.sol
@@ -37,11 +37,54 @@ contract Staking is ReentrancyGuard {
         return s_rewardPerTokenStored + (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalSupply);
     }
 
-    function stake(uint256 amount) external updateReward(msg.sender) nonReentrant moreThanZero(amount) {
+    function earned(address account) public view returns (uint256) {
+        return ((s_balances[account] * (rewardPerToken() - s_userRewardPerTokenPaid[account])) / 1e18) + s_rewards[account];
+    }
 
+    function stake(uint256 amount) external updateReward(msg.sender) nonReentrant moreThanZero(amount) {
+        s_totalSupply += amount;
+        s_balances[msg.sender] += amount;
+        emit Stake(msg.sender, amount);
+        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    function withdraw(uint256 amount) external updateReward(msg.sender) nonReentrant moreThanZero(amount) {
+        s_totalSupply -= amount;
+        s_balances[msg.sender] -= amount;
+        emit Unstake(msg.sender, amount);
+        bool success = s_stakingToken.transfer(msg.sender, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    function claimReward() external updateReward(msg.sender) nonReentrant {
+        uint256 reward = s_rewards[msg.sender];
+        s_rewards[msg.sender] = 0;
+        emit RewardClaimed(msg.sender, reward);
+        bool success = s_rewardsToken.transfer(msg.sender, reward);
+        if (!success) {
+            revert TransferFailed();
+        }
     }
 
     modifier updateReward(address account) {
-
+        s_rewardPerTokenStored = rewardPerToken();
+        s_lastUpdateTime = block.timestamp;
+        s_rewards[account] = earned(account);
+        s_userRewardPerTokenPaid[account] = s_rewardPerTokenStored;
+        _;
     }
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert NeedsMoreThanZero();
+        }
+        _;
+    }
+
+
 }
